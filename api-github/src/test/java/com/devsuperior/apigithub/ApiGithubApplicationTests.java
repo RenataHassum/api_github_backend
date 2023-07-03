fdd5fd623e6df7211b608a529b727ec039907e92
Renata Hassum <renatahassum@gmail.com>
2023-07-03 15:29:55 -0300

    Project created

---
+++ api-github/src/test/java/com/devsuperior/apigithub/ApiGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.apigithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
