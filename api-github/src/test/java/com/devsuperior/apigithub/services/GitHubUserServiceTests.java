4cb0840e3c1a4f94622284c6b8c2a666b2a9f956
Renata Hassum <renatahassum@gmail.com>
2023-07-05 14:20:41 -0300

    Service layer unit testing testGetGitHubUserDetails_ShouldReturnUserDetails OK

--- api-github/src/test/java/com/devsuperior/apigithub/services/GitHubUserServiceTests.java
+++ api-github/src/test/java/com/devsuperior/apigithub/services/GitHubUserServiceTests.java
@@ -1,11 +1,14 @@
 package com.devsuperior.apigithub.services;
 
 import com.devsuperior.apigithub.dto.GitHubUserDTO;
+import com.devsuperior.apigithub.dto.GitHubUserDetailsDTO;
 import com.devsuperior.apigithub.dto.GitHubUserPageDTO;
 import com.devsuperior.apigithub.tests.Factory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -67,6 +70,26 @@ public class GitHubUserServiceTests {
         assertEquals(mockUserList, users);
         assertEquals("http://localhost:8080/api/users?since=2", result.getNext());
 
+    }
+
+    @Test
+    public void getGitHubUserDetails_ShouldReturnUserDetails() {
+        GitHubUserDetailsDTO userDetails = new GitHubUserDetailsDTO();
+        userDetails.setId(1);
+        userDetails.setLogin("john");
+
+        ResponseEntity<GitHubUserDetailsDTO> mockResponseEntity = ResponseEntity.ok(userDetails);
+        Mockito.when(restTemplate.getForEntity(
+                        ArgumentMatchers.anyString(),
+                        ArgumentMatchers.eq(GitHubUserDetailsDTO.class)))
+                .thenReturn(mockResponseEntity);
+
+        // Execução do método a ser testado
+        GitHubUserDetailsDTO result = service.getGitHubUserDetails("john");
+
+        // Verificação dos resultados
+        Assertions.assertEquals(userDetails.getId(), result.getId());
+        Assertions.assertEquals(userDetails.getLogin(), result.getLogin());
     }
 
 
