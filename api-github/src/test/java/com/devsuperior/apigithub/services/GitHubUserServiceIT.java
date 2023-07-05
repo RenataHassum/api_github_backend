f424476f280d6cf99668361fe4776ca063f7dcca
Renata Hassum <renatahassum@gmail.com>
2023-07-05 18:04:10 -0300

    Service layer integration test: testGetGitHubUserDetails_ReturnsUserDetails OK

--- api-github/src/test/java/com/devsuperior/apigithub/services/GitHubUserServiceIT.java
+++ api-github/src/test/java/com/devsuperior/apigithub/services/GitHubUserServiceIT.java
@@ -1,11 +1,15 @@
 package com.devsuperior.apigithub.services;
 
+import com.devsuperior.apigithub.dto.GitHubUserDetailsDTO;
 import com.devsuperior.apigithub.dto.GitHubUserPageDTO;
+import com.devsuperior.apigithub.dto.GitHubUserRepositoryPageDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,4 +39,23 @@ public class GitHubUserServiceIT {
         assertNotNull(result.getNext());
         assertEquals("http://localhost:8080/api/users?since=" + result.getContent().get(result.getContent().size() - 1).getId(), result.getNext());
     }
+
+    @Test
+    public void testGetGitHubUserDetails_ReturnsUserDetails() {
+        // Arrange
+        String username = "renatahassum";
+
+        // Act
+        GitHubUserDetailsDTO expectedDetails = restTemplate
+                .getForEntity("https://api.github.com/users/" + username, GitHubUserDetailsDTO.class) //ex2 GET
+                .getBody();
+
+        GitHubUserDetailsDTO resultDto = service.getGitHubUserDetails(username);
+
+        // Assert
+        assertThat(resultDto).isNotNull();
+        assertThat(resultDto.getLogin()).isEqualTo(expectedDetails.getLogin());
+        assertThat(resultDto.getName()).isEqualTo(expectedDetails.getName());
+    }
+
 }
